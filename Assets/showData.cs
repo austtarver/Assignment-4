6d6ba1d81900414671a55c75287a04dcbba6103d
austtarver <54518526+austtarver@users.noreply.github.com>
2019-10-12 09:29:12 -0500

    initial upload

---
+++ Assets/showData.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class showData : MonoBehaviour
+{
+	public Text PanelNameText;
+	public Text PanelLivesText;
+	public Text PanelSpeedText;
+	public Text PanelScoreText;
+	public Text PanelHighScoreText;
+	public Text PanelRoundsPlayedText;
+
+	private void Awake()
+	{
+		PanelNameText.text = keepData.PlayerName;
+		PanelLivesText.text =" Lives: " + keepData.PlayerLives.ToString();
+		PanelSpeedText.text = "Speed: " + keepData.PlayerSpeed.ToString();
+		PanelHighScoreText.text = "High: " + keepData.HighScore.ToString();
+		
+		PanelRoundsPlayedText.text = "Rounds: " + keepData.RoundsPlayed.ToString();
+	}
+	private void Update()
+	{
+		PanelScoreText.text = Score.PinCount.ToString();
+	}
+}
+
